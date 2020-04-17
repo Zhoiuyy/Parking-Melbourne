2e24c49a1d23007cd061bab95a8ffdfa08d800b1
Ying Zhou <yinzhou1@student.unimelb.edu.au>
2020-04-17 17:35:05 +1000

    check routes4

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -5,32 +5,32 @@ var accountController = require('../controllers/accountController.js');
 
 
 /* home page */
-accountRouter.get('/',function(req, res, next) {
+accountRouter.get('/', function(req, res, next) {
   res.render('index', { title: 'account' });
 });
 
 accountRouter.get('/log-in',function(req, res, next) {
-    res.render('index', { title: 'log-in' });
+  res.render('index', { title: 'log-in' });
 });
 
 accountRouter.get('/sign-up',function(req, res, next) {
-    res.render('index', { title: 'sign-up' });
+  res.render('index', { title: 'sign-up' });
 });
 
 accountRouter.get('/contact',function(req, res, next) {
-    res.render('index', { title: 'contact' });
+  res.render('index', { title: 'contact' });
 });
 
 accountRouter.get('/history',function(req, res, next) {
-    res.render('index', { title: 'history' });
+  res.render('index', { title: 'history' });
 });
 
 accountRouter.get('/car-details',function(req, res, next) {
-    res.render('index', { title: 'car-details' });
+  res.render('index', { title: 'car-details' });
 });
   
 accountRouter.get('/payment-details',function(req, res, next) {
-    res.render('index', { title: 'payment-details' });
+  res.render('index', { title: 'payment-details' });
 });
 
 
