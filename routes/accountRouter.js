a999f6f8b6527d8c90c209c72b271605bdb96a28
Ying Zhou <yinzhou1@student.unimelb.edu.au>
2020-04-17 17:30:43 +1000

    check routes3

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -3,29 +3,35 @@ var accountRouter = express.Router();
 
 var accountController = require('../controllers/accountController.js');
 
+
+/* home page */
+accountRouter.get('/',function(req, res, next) {
+  res.render('index', { title: 'account' });
+});
+
 accountRouter.get('/log-in',function(req, res, next) {
     res.render('index', { title: 'log-in' });
-  });
+});
 
 accountRouter.get('/sign-up',function(req, res, next) {
     res.render('index', { title: 'sign-up' });
-  });
+});
 
 accountRouter.get('/contact',function(req, res, next) {
     res.render('index', { title: 'contact' });
-  });
+});
 
 accountRouter.get('/history',function(req, res, next) {
     res.render('index', { title: 'history' });
-  });
+});
 
 accountRouter.get('/car-details',function(req, res, next) {
     res.render('index', { title: 'car-details' });
-  });
+});
   
 accountRouter.get('/payment-details',function(req, res, next) {
     res.render('index', { title: 'payment-details' });
-  });
+});
 
 
 module.exports = accountRouter;
